4169a2a69982553a680e72a2ebb53a5cd6db9349
tytram <tytram@asu.edu>
2023-11-21 18:01:56 -0700

    Add arguemnts to bchoc file

--- bchoc.py
+++ bchoc.py
@@ -1,3 +1,19 @@
 import hashlib 
 import argparse
 import struct
+import os
+
+
+parser = argparse.ArgumentParser(description="Create Blockchain of Custody form")
+parser.add_argument("action")
+parser.add_argument('-c', help='case id') #case ID
+parser.add_argument('-i', action='append') #Item ID
+parser.add_argument('-h') #handler 
+parser.add_argument('-o') #organization
+parser.add_argument('-n') #number of entries
+parser.add_argument('-y', '-why') #reason
+parser.add_argument('-o', nargs='*') #owner Info
+
+args = parser.parse_args()
+action = args.action
+
