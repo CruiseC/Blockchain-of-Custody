04c116690a4bd535deb1213885d0e46910350496
CruiseC <cwcostin@asu.edu>
2023-11-24 18:41:45 -0700

    Attempt at second Try block

--- checkin.py
+++ checkin.py
@@ -43,3 +43,44 @@ def checkin(item_id, file_path):
             break    
             
     filepath.close()
+    
+    try:
+        
+        if state.decode('utf-8').rstrip('\x00') == "CHECKEDOUT":
+            
+            currTime = datetime.now()
+            timestamp = datetime.timestamp(currTime)
+            headVals = (previousHash, timestamp, case_id, int(item_id[0]), str.encode("CHECKEDIN"), 0)
+            dataVals = b''
+            block_data_format = struct.Struct('0s')
+            packed_headVals = block_format_head.pack(*headVals)
+            packed_dataVals = block_data_format.pack(dataVals)
+            curr_head = block_head._make(
+                block_format_head.unpack(packed_headVals))
+            curr_data = block_data._make(
+                block_data_format.unpack(packed_dataVals))
+            
+            print(curr_head)
+            print(curr_data)
+            
+            filepath = open(file_path, 'ab')
+            filepath.write(packed_headVals)
+            filepath.write(packed_dataVals)
+            filepath.close()
+
+            print("Case:", str(uuid.UUID(bytes=case_id)))
+            print("Checked in item:", item_id[0])
+            print("\tStatus:", "CHECKEDIN")
+            print("\tTime of action:", currTime.strftime(
+                '%Y-%m-%dT%H:%M:%S.%f') + 'Z')
+
+            success = True
+        
+        else:
+            Incorrect_State()
+
+    except:
+        # Item ID not found
+        Item_Not_Found()
+        
+    sys.exit(0)
