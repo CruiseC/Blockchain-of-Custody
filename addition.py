a23ed058d33c0b403d3c0aabe4ffdd488fa4c52d
tytram <tytram@asu.edu>
2023-11-24 18:38:26 -0700

    added setup for addition file

--- addition.py
+++ addition.py
@@ -6,3 +6,16 @@ import uuid
 from collections import namedtuple
 from datetime import datetime
 from errors import *
+
+def addition(case_id, item_id, handler, organization, file_path):
+    
+    success = ''
+    print_case = 0
+
+    case_id = case_id.replace("-", "")
+    rev_case_id = ""
+
+    for i in range(0, len(case_id), 2):
+        rev_case_id = case_id[i]+case_id[i+1] + rev_case_id
+
+    case_id = rev_case_id
